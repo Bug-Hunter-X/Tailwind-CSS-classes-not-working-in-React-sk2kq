819ca9b543bfbb9b896ee30749335208a84e126b
BugHunter-X <askzenai@gmail.com>
2025-01-04 23:48:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// in your main app.js file
+import 'tailwindcss/tailwind.css';
+
+// ... rest of your code
+```
+
+```html
+<div class="bg-red-500 p-4">
+  This is some text.
+</div>
+```
